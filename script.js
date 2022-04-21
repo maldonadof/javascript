6a67e7a440ae3bb7963ae13cc882d188aabe8e59
flor maldonado <flormaldonado9@outlook.com>
2022-04-21 19:55:32 -0300

    1er desafio real

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ let numero
 do {
     numero = parseFloat(prompt("Ingrese un número del 1 al 5 para una recomendación de pelicula"))
 
-    if(numero < 1) {
-        alert("Solo números del 1 al 5")
-    } else if(numero > 5) {
+    if(numero < 1 || numero > 5 || (isNaN(numero))) {
         alert("Solo números del 1 al 5")
     }
 
-} while (isNaN(numero))
+} while (numero < 1 || numero > 5 || (isNaN(numero)))
 
 switch(numero) {
     case 1:
