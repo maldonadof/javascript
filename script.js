c2001220d15ee031bcaa3400576336aacc9096ea
flor maldonado <flormaldonado9@outlook.com>
2022-04-26 21:10:22 -0300

    DesafioObligatorio1

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-
 let numero
+const valorEntrada = 870
 
 do {
-    numero = parseFloat(prompt("Ingrese un número del 1 al 5 para una recomendación de pelicula"))
+    numero = parseFloat(prompt("Ingrese un número del 1 al 5 para una recomendación de pelicula que se encuentre en cartelera"))
 
     if(numero < 1 || numero > 5 || (isNaN(numero))) {
         alert("Solo números del 1 al 5")
@@ -13,19 +13,30 @@ do {
 
 switch(numero) {
     case 1:
-        alert("Spiderman")
+        alert("Animales Fantasticos 3")
         break
     case 2:
-        alert("Dr. Strange")
+        alert("Morbius")
         break
     case 3:
-        alert("Iron Man")
+        alert("Batman")
         break
     case 4:
-        alert("Black Widow")
+        alert("Sonic 2")
         break
     case 5:
-        alert("Thor")
+        alert("La ciudad perdida")
         break
 }
 
+let personas = parseFloat(prompt("Ingrese el número de acompañantes con los que irá al cine y le diremos cuánto le saldrá"))
+
+const calcularEntrada = (monto) => monto * valorEntrada
+
+let resultado = calcularEntrada(personas)
+
+if (isNaN(personas) || personas >= 0) {
+    alert(resultado)
+} else {
+    alert("Por favor ingresá números validos")
+}
