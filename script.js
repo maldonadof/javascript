c566b6e4f1089c6c59e41d62ad4e024f3a5160bc
flor maldonado <flormaldonado9@outlook.com>
2022-05-12 21:18:19 -0300

    desafio eventos1

--- script.js
+++ script.js
@@ -14,11 +14,17 @@ const pelicula5 = new eleccionPelicula (5, "La ciudad perdida")
 const peliculas = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5]
 
 class candy {
-    constructor (nombre, precio){
+    constructor (nombre, precio, id){
         this.nombre = nombre
         this.precio = precio
+        this.id = id
     }
 }
+const candy1 = new candy("nachos", 670, 1)
+const candy2 = new candy("pochoclos", 820, 2)
+const candy3 = new candy("panchos", 500, 3)
+
+eleccionCandy = [candy1, candy2, candy3]
 
 
 let numero
@@ -51,22 +57,9 @@ formCantidad.addEventListener('submit', (event) => {
     
 
     //elección de producto del candy.
-    do {
-        acompaniamiento =  document.getElementById('idAcompaniamiento').value    
-    } while (acompaniamiento < 1 || acompaniamiento > 3 || (isNaN(acompaniamiento)))
-    
-    switch(acompaniamiento) {
-        case 1:
-            montoFinal.push(new candy("nachos", 670))
-            break
-        case 2:
-            montoFinal.push(new candy("pochoclos", 820))
-            break
-        case 3:
-            montoFinal.push (new candy("panchos", 500))
-            break
-    }
-    //función para el monto total más mensaje de despedida.
+    acompaniamiento =  document.getElementById('idAcompaniamiento').value
+    acompaniamientoSeleccionado = eleccionCandy.find(candy=>candy.id == acompaniamiento)
+    montoFinal.push(acompaniamientoSeleccionado)
 
     precioTotal = montoFinal.reduce ((valPrevio, valActual) => valPrevio + valActual.precio, 0)
 
